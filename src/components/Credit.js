61e4c80f315418b799f7b40852050417f54276e7
Karthik Kumar Badri <86832221+badrikarthik16@users.noreply.github.com>
2024-06-15 10:43:44 +0000

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/badrikarthik16"
+        > Karthik Badri</a>
+    </p>
+  </div>
+  );
+}
